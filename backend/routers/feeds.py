bc18f8d1cb130c636f2ad753081c1268da6b0232
Pierre-Yves Gillier <github@pygillier.me>
2023-01-22 00:20:08 +0100

    Feeds: Create.

--- backend/routers/feeds.py
+++ backend/routers/feeds.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from backend.database import crud
-from backend.dependencies.db import get_db
+from backend.database import crud, schemas
+from backend.dependencies import db, security
 import logging
 from backend.dependencies import security
 
@@ -9,20 +9,25 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="/feeds",
-    tags=["Feeds"],
-    dependencies=[Depends(security.oauth2_scheme)]
+    tags=["Feeds"]
 )
 
-@router.post("/")
-async def create_feed():
+
+@router.post("/", response_model=schemas.Feed)
+async def create_feed(
+        feed: schemas.FeedCreate,
+        current_user: schemas.User = Depends(security.get_current_active_user),
+        sess: Session = Depends(db.get_db)
+):
     logger.info("Inserting new feed")
-    pass
+    feed = crud.create_user_feed(db=sess,feed=feed, user_id=current_user.id)
+    return feed
 
 
 @router.get("/")
-async def read_feeds(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_feeds(skip: int = 0, limit: int = 100, sess: Session = Depends(db.get_db)):
     logger.info("Getting all feeds")
-    feeds = crud.get_feeds(db, skip=skip, limit=limit)
+    feeds = crud.get_feeds(sess, skip=skip, limit=limit)
     return feeds
 
 
